c0ed378b6594fd2b817c8b7aee4c09a49d6607c9
MatisGalvin <72927745+MatisGalvin@users.noreply.github.com>
2023-03-13 13:44:17 +0100

    Revert "Dev"

--- src/components/MainTitle/MainTitle.jsx
+++
@@ -1,8 +0,0 @@
-export default function MainTitle({title, subtitle}){
-    return(
-        <section className="w-screen px-20 flex flex-col items-start py-9">
-            <h1>{title}</h1>
-            <p className="h-8 font-normal text-lg text-light">{subtitle}</p>
-        </section>
-    )
-}
