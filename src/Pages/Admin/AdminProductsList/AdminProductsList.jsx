e1d7671ad70756316b40f1ff71cfaa01573677c2
Maxime BAUDOIN <maxime.baudoin@epsi.fr>
2023-03-15 09:52:50 +0100

    CRUD Produit

---
+++ src/Pages/Admin/AdminProductsList/AdminProductsList.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { FeteDeLaMusique } from "../../../api/feteDeLaMusique";
+import { Section } from "../../../components/Section/Section";
+import { ModalDelete } from "../../../components/ModalDelete/ModalDelete";
+
+export function AdminProductsList() {
+
+    const [productList, setProductList] = useState();
+    const [modalContent, setModalContent] = useState();
+    const [modalState, setModalState] = useState(false);
+
+    async function fetchProducts() {
+        let resultFetch = await FeteDeLaMusique.fetchAllProducts();
+        setProductList(resultFetch);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    function handleDelete(e) {
+        setModalState(e.target.dataset.id);
+        setModalContent('Êtes-vous certains de vouloir supprimer ce produit ?');
+    }
+
+    async function onDelete(e) {
+        let resultDelete = await FeteDeLaMusique.deleteProductById(e.target.dataset.productId);
+        console.log(resultDelete);
+
+        if(resultDelete) {
+            setModalState(false);
+            setModalContent(false);
+
+            setProductList(productList.filter((element) => {
+                console.log(element);
+                if(element.id == e.target.dataset.productId) {
+                    return false;
+                }
+                return true;
+            }));
+        }
+    }
+
+    function onCancel() {
+        setModalState(false);
+        setModalContent(false);
+    }
+
+    return <Section className="mx-10 rounded-lg overflow-x-auto border border-gray-200">
+        <table className="table-auto w-full text-sm text-left text-gray-500">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 border-b border-gray-200">
+                <tr>
+                    <th scope="col" className="px-6 py-3">
+                        Product name
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Price
+                    </th>
+                    <th scope="col" className="px-6 py-3">
+                        Actions
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {productList && productList.map((product, i) => (
+                    <tr key={product.name + i} className="bg-white even:bg-gray-50">
+                        <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">{product.name}</td>
+                        <td className="px-6 py-4">{product.price/100}</td>
+                        <td>
+                            <a href={`/admin/product/update/${product.id}`} className="font-bold text-primary cursor-pointer hover:underline">Modifier</a>
+                            <div data-id={product.id} onClick={handleDelete} className="font-bold text-red-900 cursor-pointer hover:underline">Supprimer</div>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+
+        {modalState && <ModalDelete onConfirm={onDelete} onCancel={onCancel} content={modalContent} productId={modalState} />}
+    </Section>
+}
--- src/index.js
+++ src/index.js
@@ -9,21 +9,32 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Events } from "./Pages/Events/Events";
 import { Products } from "./Pages/Products/Products";
 import CartPage from "./Pages/Cart/CartPage";
+import { AdminProductsList } from "./Pages/Admin/AdminProductsList/AdminProductsList";
+import { AppAdmin } from "./AppAdmin";
+import { AdminProductUpdate } from "./Pages/Admin/AdminProductUpdate/AdminProductUpdate";
+import { AdminProductAdd } from "./Pages/Admin/AdminProductAdd/AdminProductAdd";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/events" element={<Events />}></Route>
-          <Route path="/products" element={<Products />}></Route>
-          <Route path="/mycart" element={<CartPage />}></Route>
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/admin" element={<AppAdmin />}>
+                    <Route path="" element={<PageNotFound />}></Route>
+                    <Route path="products" element={<AdminProductsList />}></Route>
+                    <Route path="product/update/:id" element={<AdminProductUpdate />}></Route>
+                    <Route path="product/add" element={<AdminProductAdd />}></Route>
+                    <Route path="*" element={<PageNotFound />}></Route>
+                </Route>
+                <Route path="/" element={<App />}>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/events" element={<Events />}></Route>
+                    <Route path="/products" element={<Products />}></Route>
+                    <Route path="/mycart" element={<CartPage />}></Route>
+                    <Route path="*" element={<PageNotFound />}></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>
 );
